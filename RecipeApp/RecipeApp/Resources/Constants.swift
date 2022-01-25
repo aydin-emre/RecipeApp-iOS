518c3d33162a2578edd1d0fa291d1372274df8ed
Emre Huawei <ea@ea.tc>
2022-01-25 23:01:00 +0300

    Some folders created..

---
+++ RecipeApp/RecipeApp/Resources/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  RecipeApp
+//
+//  Created by Emre on 25.01.2022.
+//
+
+import UIKit
+
+let appColor = UIColor(red: 2/255.0, green: 119/255.0, blue: 189/255.0, alpha: 1)
